9db0ce00af730b8848d24f9fd5cb953d278ddcd8
Dranderson <dranderson@DrandersondeMacBook-Air.local>
2017-09-30 09:30:30 -0500

    move template to static folders

--- index.py
+++ index.py
@@ -12,7 +12,7 @@ import jinja2
 import webapp2
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    loader=jinja2.FileSystemLoader([os.path.dirname(__file__), '/static_files/views']),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 # [END imports]
