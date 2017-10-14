fa98ff85fec31c9cb6e032ca86e08205cf6315e7
MoezGholami <Ali.Moez.Gholami@GMail.COM>
2017-10-13 19:19:23 -0500

    fix map for firefox (done by ming)

--- static_files/js/stream.js
+++ static_files/js/stream.js
@@ -68,10 +68,10 @@ function refresh() {
 $(function() {
   $( "#slider-range" ).slider({
     range: true,
-    min: (new Date().getTime() / 1000) - 31536000,
-    max: (new Date().getTime() / 1000),
+    min: (new Date().getTime() / 1000) - 31536000 + 86400,
+    max: (new Date().getTime() / 1000) + 86400,
     step: 86400,
-    values: [ new Date('2017-01-01').getTime() / 1000, (new Date().getTime() / 1000)  ],
+    values: [ new Date('2017-01-01').getTime() / 1000, (new Date().getTime() / 1000) + 86400 ],
     slide: function( event, ui ) {
       $( "#amount" ).val( (new Date(ui.values[ 0 ] *1000).toDateString() ) + " - " + (new Date(ui.values[ 1 ] *1000)).toDateString() );
       date_min = ui.values[ 0 ] * 1000;
@@ -94,11 +94,9 @@ function filter() {
   filtered_photos = new Array();
   for(var i = 0; i < photo_objs.length; i++) {
     var p_date = photo_objs[i].date;
-    var date_value = new Date(p_date.substring(0, 19) + " GMT-0000").getTime();
+    var date_value = new Date(p_date.substring(0, 19)).getTime();
     if(date_value >= date_min && date_value <= date_max) {
       filtered_photos.push(photo_objs[i]);
     }
   }
 }
-
-
